699edd925e6f8b94d22857a1f7aa8b442b2297e4
Remco Bloemen <remco@0x.org>
2019-04-21 19:26:40 -0700

    Add Abstract Syntax Tree

--- parser/src/AST.rs
+++ parser/src/AST.rs
@@ -1,12 +1,25 @@
+use serde::{Deserialize, Serialize};
 
-struct Identifier(String);
+// An identifier occupies a binder spot.
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Debug)]
+pub struct Binder(pub String);
 
-enum Expression {
-
+// An expression occupies a reference spot.
+// Fructose is an inline declaration in parenthesis. It occupies one reference
+// spot which is linked to one implicit binding spot.
+// Galactose is a call statement in parenthesis.
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Debug)]
+pub enum Expression {
+    Reference(String),
+    Fructose(Vec<Binder>, Vec<Expression>),
+    Galactose(Vec<Expression>),
+    Literal(String),
 }
 
-enum Statement {
-    Closue(Vec<Identifier>, Vec<Expression>)
-    Call(Vec<Expression>)
-    Block(Vec<Statement>)
+// Glucose is a closure with an empty Call followed by a Call on the next line.
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Debug)]
+pub enum Statement {
+    Closure(Vec<Binder>, Vec<Expression>),
+    Call(Vec<Expression>),
+    Block(Vec<Statement>),
 }
